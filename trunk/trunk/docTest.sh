c0fd0a7bb5fc1169771d74434de2d66fd8850d41
sojamo <sojamo@ae107e73-614b-0410-a23d-3377eb54eec6>
2008-04-23 09:57:30 +0000

    starting sojamo libraries repository.

---
+++ trunk/trunk/docTest.sh
@@ -0,0 +1,10 @@
+########################################################
+# generate sojamoP5 distribution
+# 2006 by andreas schlegel 
+# $JAVA is the path to the java 1.5 commands folder
+#
+########################################################
+
+
+
+javadoc -docletpath /Users/andreas/Desktop/XHTML_Doclet_0.4.jar -doclet net.sourceforge.xhtmldoclet.Doclet -sourcepath ../../b/source sojamo.drop
