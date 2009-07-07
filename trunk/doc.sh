f1c3ea976b4ec630556263213e97ca4ed749fe79
sojamo <sojamo@ae107e73-614b-0410-a23d-3377eb54eec6>
2009-07-07 06:35:05 +0000

    updating build and doc scripts.

--- trunk/doc.sh
+++ trunk/doc.sh
@@ -8,6 +8,7 @@
 
 
 javadoc -doclet prodoc.StartDoclet -docletpath ../../libs/prodoc/  -sourcepath ./src sojamo.drop
+javadoc -doclet prodoc.StartDoclet -docletpath ../../libs/prodoc/  -sourcepath ./src sojamo.http
 
 #javadoc -doclet prodoc.StartDoclet -docletpath ../../libs/prodoc/  -sourcepath ./source sojamo.net
 #javadoc -doclet prodoc.StartDoclet -docletpath ../../libs/prodoc/  -sourcepath ./source sojamo.osc
@@ -29,6 +30,16 @@ cp -r ./src/sojamo/drop/docuImages ./jar/sDrop/reference
 cp -r ./src/sojamo/drop/examples ./jar/sDrop/
 
 
+mkdir ./jar
+mkdir ./jar/sHTTP
+mkdir ./jar/sHTTP/library
+cp ./jar/sHTTP.jar ./jar/sHTTP/library/
+cp -r ./src/sojamo/http/documentation ./jar/sHTTP/
+mv ./jar/sHTTP/documentation ./jar/sHTTP/reference
+cp -r ./src/sojamo/http/docuImages ./jar/sHTTP/reference
+cp -r ./src/sojamo/http/examples ./jar/sHTTP/
+
+
 ########################################################
 # distribution
 ########################################################
@@ -45,6 +56,17 @@ cd ./distribution
 zip -r sDrop sDrop
 cd ..
 
+
+cp -r jar/sHTTP distribution
+find ./distribution -name .DS_Store -ls -exec rm {} \;
+find ./distribution -name .svn -ls -exec rm -rf {} \;
+tar cvzf ./distribution/sHTTP.tgz ./distribution/sHTTP
+cd ./distribution
+zip -r sHTTP sHTTP
+cd ..
+
+
+
 find . -name .DS_Store -ls -exec rm {} \;
 
 ########################################################
@@ -57,5 +79,5 @@ mv source ../sojamoCompiled
 mv build ../sojamoCompiled
 mv distribution ../sojamoCompiled
 
-rm -r ./src/sojamo/drop/reference
-
+rm -r ./src/sojamo/drop/documentation
+rm -r ./src/sojamo/http/documentation
